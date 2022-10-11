7c453014ff486f29ad140c12abcdfd997051965c
Xaria A <xaria.shella@gmail.com>
2022-10-11 09:52:59 -0500

    basic foundation of house

--- src/ScaryHouse.java
+++ src/ScaryHouse.java
@@ -17,7 +17,49 @@ public class ScaryHouse extends Canvas
 
    public void paint( Graphics window )
    {
-      //call theHouse
+      window.setColor(Color.BLACK);
+      window.drawString("Happy Halloween", 100, 50);
+
+      //columns and outlines
+      window.setColor(Color.MAGENTA);
+      window.fillRect(100, 250, 100, 100 );
+
+      window.setColor(Color.BLACK);
+      window.drawRect(100,250,100,100);
+
+      window.setColor(Color.MAGENTA);
+      window.fillRect(200, 170, 100, 180 );
+
+      window.setColor(Color.BLACK);
+      window.drawRect(200,200,100,150);
+
+      window.setColor(Color.MAGENTA);
+      window.fillRect(300, 250, 100, 100 );
+
+      window.setColor(Color.BLACK);
+      window.drawRect(300,250,100,100);
+
+
+      //roof
+      window.setColor(Color.BLACK);
+      window.fillRect(100, 230, 100, 30 );
+
+      window.setColor(Color.BLACK);
+      window.fillRect(200, 160, 100, 50 );
+
+      window.setColor(Color.BLACK);
+      window.fillRect(300, 230, 100, 30 );
+
+
+
+      /*window.setColor(Color.RED);
+      window.fillRect(100,200,40,40);
+
+      window.setColor(Color.BLUE);
+      window.drawRect(100,250,20,40);
+
+      window.setColor(Color.ORANGE);
+      window.fillRect(100,300,90,20);*/
    }
 
    public void theHouse( Graphics window )
